d187540a79ed4f2a33af4fa31a9c22934331ba90
Drew Castalia <dcastalia@pima.edu>
2019-02-06 10:52:50 -0700

    Added deterministic option to particle spawning.

--- Assets/Scripts/NetworkedObjects.cs
+++ Assets/Scripts/NetworkedObjects.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 using Photon.Pun;
 
@@ -14,6 +15,8 @@ public class NetworkedObjects : MonoBehaviour
 
 	public static NetworkedObjects find;
 
+	int seed; // only matters on the master client
+
 	// singleton assignment
 	void Awake() {
 		find = this;
@@ -21,9 +24,14 @@ public class NetworkedObjects : MonoBehaviour
 
 	void Start() {
 
+		if( PhotonNetwork.IsMasterClient ) {
+			// try to create a truly random number to use as your starting random seed
+			seed = DateTime.Now.Millisecond + System.Threading.Thread.CurrentThread.GetHashCode();
+		}
+
 		// when the game starts on this client, instantiate a player from a named prefab in the resources folder
-		float xRange = Random.Range( -world.bounds.extents.x, world.bounds.extents.x );
-		float yRange = Random.Range( -world.bounds.extents.x, world.bounds.extents.x );
+		float xRange = UnityEngine.Random.Range( -world.bounds.extents.x, world.bounds.extents.x );
+		float yRange = UnityEngine.Random.Range( -world.bounds.extents.x, world.bounds.extents.x );
 
 		Vector3 spawnPos = world.bounds.center + new Vector3( xRange, yRange, 0f );
 		PhotonNetwork.Instantiate( "Player", spawnPos, Quaternion.identity, 0 );
@@ -33,9 +41,10 @@ public class NetworkedObjects : MonoBehaviour
 		// add a player to the list of all tracked players
 		players.Add( player );
 
-		// only the "server" has authority over which color the player should be
+		// only the "server" has authority over which color the player should be and its seed
 		if( PhotonNetwork.IsMasterClient ) {
 			player.RPC( "SetColor", RpcTarget.AllBuffered, players.Count - 1 ); // buffer the color change so it applies to new arrivals in the room
+			player.RPC( "SetRandomSeed", RpcTarget.AllBuffered, seed );
 		}
 	}
 }
--- Assets/Scripts/PlayerParticle.cs
+++ Assets/Scripts/PlayerParticle.cs
@@ -10,6 +10,8 @@ public class PlayerParticle : MonoBehaviourPun
 	public Transform spawnTarget;
 	public PlayerColor playerColor;
 
+	System.Random random;
+
 	void Update() {
 		// if this client doesn't control this player, then don't let it spawn particles on input
 		if( !photonView.IsMine ) return;
@@ -21,9 +23,17 @@ public class PlayerParticle : MonoBehaviourPun
 	}
 
 	[PunRPC]
+	public void SetRandomSeed( int newSeed ) {
+		random = new System.Random( newSeed );
+	}
+
+	[PunRPC]
 	public void SpawnParticle() {
 		// pick a random particle (note how the randomness means that a different particle might be picked on each client)
-		ParticleSystem particlePrefab = particlePrefabs[ Random.Range(0, particlePrefabs.Count) ];
+		ParticleSystem particlePrefab = particlePrefabs[ UnityEngine.Random.Range(0, particlePrefabs.Count) ];
+
+		// pick a random particle (using a deterministic algorithm)
+		// ParticleSystem particlePrefab = particlePrefabs[ random.Next(0, particlePrefabs.Count) ];
 		ParticleSystem newSystem = Instantiate<ParticleSystem>( particlePrefab, spawnTarget.position, Quaternion.identity );
 
 		// set the particle to be the same color as the player so it's identifiable as theirs
